b8bfc49c52256f65bb671d5f1a812976d9e43d66
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-12-27 19:05:32 +0900

    fix:lint

--- src/layout/FluidLayout.tsx
+++ src/layout/FluidLayout.tsx
@@ -5,7 +5,7 @@ import { LayoutErrorBoundary } from "src/layout/LayoutErrorBoundary";
 
 export const FluidLayout: CustomLayout = (page) => {
   return (
-    <div className="grid grid-rows-[auto,1fr,auto] min-h-screen">
+    <div className="grid min-h-screen grid-rows-[auto,1fr,auto]">
       <Header />
       <main className="px-4 bg-gray-300">
         <LayoutErrorBoundary>{page}</LayoutErrorBoundary>
