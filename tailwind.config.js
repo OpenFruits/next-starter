cf742f2fc553b3a4fd33dd0e4378f95aeb96c724
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-01-12 19:01:42 +0900

    🔧

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  darkMode: false, // 'media' or 'class'
   theme: { extend: {} },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography"), require("@tailwindcss/line-clamp")],
 };
