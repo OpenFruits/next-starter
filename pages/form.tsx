130628d4b2daa6adcc7e7742d4f68ba933ad7df8
taku1115 <59477643+taku1115@users.noreply.github.com>
2022-03-12 23:59:07 +0900

    ✨ feat example form

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,13 +1,44 @@
 import type { CustomNextPage } from "next";
 import Head from "next/head";
+import type { SubmitHandler } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { FixedLayout } from "src/layout/FixedLayout";
 
+type Inputs = {
+  example: string;
+  exampleRequired: string;
+};
+
 const Form: CustomNextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => alert(data);
+
+  // eslint-disable-next-line no-console
+  console.log(watch("example")); // watch input value by passing the name of it
+
   return (
     <>
       <Head>
         <title>Form Page</title>
       </Head>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* register your input into the hook by invoking the "register" function */}
+          <input defaultValue="test" {...register("example")} />
+          {/* include validation with required or other standard HTML validation rules */}
+          <input {...register("exampleRequired", { required: true })} />
+          {/* errors will return when field validation fails  */}
+          {errors.exampleRequired && <span>This field is required</span>}
+
+          <input type="submit" />
+        </form>
+      </div>
     </>
   );
 };
